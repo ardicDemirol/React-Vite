56c08264da47bff1fefd8ae85eb1d17cf138238c
ardicDemirol <ardc.dmrol@gmail.com>
2025-05-16 12:54:38 +0300

    useReducer hook added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@
 // import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
 // import Example from "./tutorial/04-project-structure/starter/Example";
 // import List from "./tutorial/05-leverage-javascript/starter/List";
-import Final from "./tutorial/09-context-api/final/context/Navbar";
-import Starter from "./tutorial/09-context-api/starter/Navbar";
+import Starter from "./tutorial/10-useReducer/starter/01-useReducer";
 
 function App() {
   return (
     <div className="container">
-      <Final />
       <Starter />
       {/* <Home /> */}
       {/* <About /> */}
